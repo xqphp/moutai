c2b7a7a636db66fe46c221fec3f7209a1a86033c
小七 <103240733+xqphp@users.noreply.github.com>
2023-04-16 21:09:40 +0800

    优化下输出!

--- moutai_price.py
+++ moutai_price.py
@@ -1,15 +1,20 @@
-import requests,time,xlwt,os
+import requests,time,xlwt,os,base64
 headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
 today_time=time.strftime('%Y-%m-%d', time.localtime())
+
 def moutai_data():
+    url = 'aHR0cHM6Ly9wYXltZW50Lm5qeWh3bC50b3AvYXBpL3Byb3h5L3B1YmxpYy9hcGkvcHJpY2Uvc2VsZWN0UGFnZT9wYWdlPQ=='
     try:
-        url="https://payment.njyhwl.top/api/proxy/public/api/price/selectPage?page=1"
-        r=requests.get(url,headers=headers,timeout=7)
+        url=base64.b64decode(url).decode("utf-8")
+        r=requests.get(url+'1',headers=headers,timeout=7)
         r.encoding=r.apparent_encoding
-        mt_data=r.json()['content']
+        r2 = requests.get(url + '2', headers=headers, timeout=7)
+        r2.encoding = r2.apparent_encoding
+        mt_data = r.json()['content'] +r2.json()['content']
         return  mt_data
     except Exception as e:
         print(f'接口失效!或者网络问题!\n{e}')
+
 def moutai_Excel():
     if not os.path.exists(f'茅台今日行情-{today_time}.xls'):
         print('正在生成Excel请稍后*********')
